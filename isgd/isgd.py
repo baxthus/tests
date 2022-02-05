358d039f4d9bde4e84e639c7738d55f024bd809b
Abysmal <ritzyvex684@protonmail.com>
2022-02-04 21:31:38 -0300

    isgd updated

--- isgd/isgd.py
+++ isgd/isgd.py
@@ -5,13 +5,13 @@ import gdshortener
 
 servico = input('is.gd ou v.gd: ')
 
-if servico == 'is.gd' or 'is':
+if servico == 'is.gd':
     url = input('Digite a url: ')
     custom_url = input('Digite a url customizada: ')
     s = gdshortener.ISGDShortener()
     s.shorten(url, custom_url)
     print('is.gd/' + custom_url)
-elif servico == 'v.gd' or 'v':
+elif servico == 'v.gd':
     url = input('Digite a url: ')
     custom_url = input('Digite a url customizada: ')
     s = gdshortener.VGDShortener()
